8232864536e97caf493cda2ed2d6a92d237ec91c
Kibet <nesh@localhost.localdomain>
2024-07-09 21:31:46 +0300

    Loading files...

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -5,7 +5,9 @@ import asyncio
 import time
 from importlib import import_module as using
 
+
 async_comprehension = using('1-async_comprehension').async_comprehension
+
 
 async def measure_runtime() -> float:
     '''Executes async_comprehension 4 times and measures the
